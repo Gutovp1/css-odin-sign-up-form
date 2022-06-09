03ff07d848a6a91a415beb86b733aa23639448a4
Augusto Ventura <paivaav@gmail.com>
2022-06-09 14:42:43 -0300

    insert image credit links and passwords match verification

--- javascript.js
+++ javascript.js
@@ -1,2 +1,11 @@
-const pass = document.getElementById("pass");
-const confirmation = document.getElementById('confirm-pass')
+const pass = document.getElementById('pass');
+const confirmation = document.getElementById('confirm-pass');
+
+function checkPass(){
+    if(pass.value != confirmation.value)
+    {
+        confirmation.setCustomValidity('Passwords do not match.');
+    }else{
+        confirmation.setCustomValidity('');
+    }
+}
